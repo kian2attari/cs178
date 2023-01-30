6b2b4e59555b17bfc2369d5174e27f48fbe916df
Kian Attari <kianattari@college.harvard.edu>
2023-01-29 20:55:47 -0500

    waay more attribs identified

--- hw1/todo-app/src/App.svelte
+++ hw1/todo-app/src/App.svelte
@@ -2,6 +2,7 @@
   // #Component - the todo app itself is a component than can be reused in other projects
   // The app could be broken down into smaller components, but since the app is simple,
   // I've kept it all in one file
+  let appTitle = 'ToDo';
   let todoItems = [];
   let newTodo = '';
 
@@ -15,7 +16,7 @@
       id: Date.now(),
     };
 
-    // #Reactive
+    // #ReactiveDeclaration
     // Because Svelte's reactivity is based on assignments, using array methods like
     // .push() and .splice() won't automatically trigger updates
     // We use the spread operator and reassignment below to trigger the update and take advantage of
@@ -28,21 +29,28 @@
 <main>
   <!-- This code just outlines the basic structure/layout and style of the app component (HTML/CSS) -->
   <div class="container">
-    <h1 class="app-title">ToDo!</h1>
+    <!-- #text-exp -->
+    <!-- The "Tasks" could just be called whatever the app name is -->
+    <!-- The Text Expressions concept helps reduce repetition -->
+    <h1 class="app-title">{appTitle}!</h1>
     <ul class="todo-list" />
     <div class="empty-state">
       <svg class="checklist-icon"><use href="#checklist-icon" /></svg>
       <!-- This should only appear when there are no entries -->
-      <h2 class="empty-state__title">Add your first task</h2>
-      <p class="empty-state__description">Let's get some things done!</p>
+      <!-- #text-exp -->
+      <h2 class="empty-state__title">Add your first {appTitle}</h2>
+      <!-- #text-exp -->
+      <p class="empty-state__description">Let's get some {appTitle}s done!</p>
     </div>
     <!-- #EventHandler Specifies the function that should run on the submit event with addTodo() as the handler  -->
     <form on:submit|preventDefault={addTodo}>
       <!-- #Binding We bind the prop value to the reactive variable newTodo -->
+      <!-- #text-exp in the value attribute's value  -->
+      <!-- #Attributes (ex. value attribute) -->
       <input
         class="js-todo-input"
         type="text"
-        aria-label="Add a new task to do"
+        aria-label="Add a new {appTitle}"
         placeholder="E.g. Eat breakfast"
         bind:value={newTodo}
       />
