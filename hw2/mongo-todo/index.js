821cb357706db0c253f0cbd0d3936642c528f1c9
Kian Attari <kianattari@college.harvard.edu>
2023-02-13 16:26:05 -0500

    backend connection up

--- hw2/mongo-todo/index.js
+++ hw2/mongo-todo/index.js
@@ -1,15 +1,28 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
 const app = express();
+dotenv.config();
+
+//connection to db
+
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
+    console.log("Connected to db!");
+    app.listen(3000, () => console.log("Server Up and running"));
+});
 
 app.set("view engine", "ejs");
 app.use("/static", express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-app.listen(3000, () => console.log("Server Up and running"));
+
 
 // #Routing
 app.get('/',(req, res) => {
     // #Template
     res.render('todo.ejs');
 });
+
+
 
 app.post('/',(req, res) => {console.log(req.body);});
