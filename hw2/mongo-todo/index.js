0079db090bd3d871795a1f224e0ac1d1b0fc6e6e
Kian Attari <kianattari@college.harvard.edu>
2023-02-13 16:31:39 -0500

    post works

--- hw2/mongo-todo/index.js
+++ hw2/mongo-todo/index.js
@@ -1,6 +1,8 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const mongoose = require("mongoose");
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+//models
+const TodoTask = require('./models/TodoTask');
 
 const app = express();
 dotenv.config();
@@ -8,21 +10,27 @@ dotenv.config();
 //connection to db
 
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
-    console.log("Connected to db!");
-    app.listen(3000, () => console.log("Server Up and running"));
+    console.log('Connected to db!');
+    app.listen(3000, () => console.log('Server Up and running'));
 });
 
-app.set("view engine", "ejs");
-app.use("/static", express.static("public"));
+app.set('view engine', 'ejs');
+app.use('/static', express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-
 // #Routing
-app.get('/',(req, res) => {
+app.get('/', (req, res) => {
     // #Template
     res.render('todo.ejs');
 });
 
-
-
-app.post('/',(req, res) => {console.log(req.body);});
+//POST METHOD
+app.post('/', async (req, res) => {
+    const todoTask = new TodoTask({ content: req.body.content });
+    try {
+        await todoTask.save();
+        res.redirect('/');
+    } catch (err) {
+        res.redirect('/');
+    }
+});
