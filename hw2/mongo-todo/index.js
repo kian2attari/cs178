17fd0e85a04d362702aeff2270ae5fe35fec5515
Kian Attari <kianattari@college.harvard.edu>
2023-02-13 16:08:42 -0500

    upto GET

--- hw2/mongo-todo/index.js
+++ hw2/mongo-todo/index.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+app.use("/static", express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 app.listen(3000, () => console.log("Server Up and running"));
+
+// Routing
+app.get('/',(req, res) => {
+    res.render('todo.ejs');
+});
