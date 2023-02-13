5887c5adcd809300061a928084abd65dff72432a
Kian Attari <kianattari@college.harvard.edu>
2023-02-13 16:10:20 -0500

    post done

--- hw2/mongo-todo/index.js
+++ hw2/mongo-todo/index.js
@@ -6,7 +6,10 @@ app.use("/static", express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.listen(3000, () => console.log("Server Up and running"));
 
-// Routing
+// #Routing
 app.get('/',(req, res) => {
+    // #Template
     res.render('todo.ejs');
 });
+
+app.post('/',(req, res) => {console.log(req.body);});
